1b13364a98d92708b49ac2b54966940a853afcb8
Shaquille Galimba <shaqrivergalimba24@gmail.com>
2020-02-21 07:24:27 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,27 @@
 # Add your code here
+class Dog
+  attr_accessor :name
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    save
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.clear_all
+    @@all.clear
+  end
+
+  def self.print_all
+    self.all.each {|i| puts i.name}
+  end
+
+  def save
+    @@all << self
+  end
+end
